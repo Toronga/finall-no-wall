bd8acc680df66034271c6949434315bb1ff48d70
Toronga <dillontoronga@gmail.com>
2023-05-29 16:54:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+$('.slick-carousel').slick({
+    infinite: true,
+    slidesToShow: 3, // Shows a three slides at a time
+    slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+    dots: true // Adds the dots on the bottom
+  });
+  
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
